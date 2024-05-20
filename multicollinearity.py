f1f07c3cf95cdf6a22ea6e758ab02f2599127c6b
Penosh22 <142080192+Penosh22@users.noreply.github.com>
2024-05-20 12:49:58 +0530

    Update multicollinearity.py

--- multicollinearity.py
+++ multicollinearity.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 import statsmodels.api as sm
 
@@ -27,13 +28,17 @@ X = df[features]
 y = df['Close']
 
 if len(features) > 0:
-    # Calculate VIF
-    vif = calculate_vif(X)
+    # MinMax scaling
+    scaler = MinMaxScaler()
+    X_scaled = pd.DataFrame(scaler.fit_transform(X), columns=features)
+    
+    # Calculate VIF on scaled data
+    vif = calculate_vif(X_scaled)
     st.write(vif)
 
-    # Fit a linear regression model
-    X = sm.add_constant(X)
-    model = sm.OLS(y, X).fit()
+    # Fit a linear regression model on scaled data
+    X_scaled = sm.add_constant(X_scaled)
+    model = sm.OLS(y, X_scaled).fit()
 
     # Display model summary
     st.text(model.summary())
